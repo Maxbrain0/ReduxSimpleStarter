c01797832fcd822593d3da6d7d25070ce5ddbddf
Jacob Goodwin <jacob.goodwin@gmail.com>
2017-09-05 22:23:59 -0700

    Complete lesson 43 - connect to book through redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer //books is whatever is returned from books BooksReducer
+                      //It is not the function, but the returned value (I think)
 });
 
 export default rootReducer;

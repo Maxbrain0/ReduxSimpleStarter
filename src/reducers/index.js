cafc1dc7d1d6fc347ede0f6f9fdf52f7e0b7461e
Jacob Goodwin <jacob.goodwin@gmail.com>
2017-09-07 21:22:59 -0700

    Add comments to aid newb

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 import ActiveBook from './reducer_active_book';
+
 const rootReducer = combineReducers({
-  books: BooksReducer, 
+  // these combine to make the state
+  // reducer is responsible for creating state
+  // BooksReducer creates state for books
+  books: BooksReducer,
   activeBook: ActiveBook
 });
 
--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,5 +1,8 @@
 // state argument is not application state, but only the state this
 // reducer is responsible for
+
+// reducers are in charge of changing state over time
+// they do this through actions
 export default function(state = null, action) {
   switch(action.type) {
     case 'BOOK_SELECTED':
